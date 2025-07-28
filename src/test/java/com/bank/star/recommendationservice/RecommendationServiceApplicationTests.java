a7f5e8587e90dd335199c280c02c967d242e6986
WalterMitty23 <23alexold23@gmail.com>
2025-07-28 06:17:35 -0400

    Initial project setup: Spring Boot + JDBC + H2

---
+++ src/test/java/com/bank/star/recommendationservice/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.star.recommendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
